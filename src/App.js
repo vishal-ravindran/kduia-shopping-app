8e6dbbfa4f1fb2493d037609909ccd3861034c2f
vishal ravindran <vishalravindran.student@gmail.com>
2024-02-23 18:07:09 -0500

    resolved cartvalue issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
 
-function App() {
-  return (
-    <AppProvider>
+const App = () => {
+    return (
+        <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Shopping App</h1>
                 <div className='row mt-3'> 
@@ -33,7 +35,6 @@ function App() {
                 </div>
             </div>
         </AppProvider>
-  );
-}
-
+    );
+};
 export default App;
